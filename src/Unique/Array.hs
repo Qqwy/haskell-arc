7c09f42f5464ed018d00d0dee50f4548353e43ed
qqwy <qqwy@gmx.com>
2023-10-26 18:49:44 +0200

    Faithful implementations of Consumable and Dupable
    which will still work if an Array contains other non-movable types

--- src/Unique/Array.hs
+++ src/Unique/Array.hs
@@ -7,6 +7,8 @@
 {-# LANGUAGE UnliftedNewtypes #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# OPTIONS_GHC -Wno-redundant-constraints #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# OPTIONS_GHC -Wno-name-shadowing #-}
 
 -- | Uniqueness-based mutable arrays which do not depend on fusion for efficiency.
 --
@@ -103,7 +105,7 @@ data Array a = Array (Array# a)
 newtype Array# a = Array# (GHC.MutableArray# GHC.RealWorld a)
 
 -- | [Unique constructor]("Unique#unique_constructor") to create an `Array` from a list.
-fromList :: Unique witness => [a] %1 -> witness %1 -> (Array a, witness)
+fromList :: (HasCallStack, Unique witness) => [a] %1 -> witness %1 -> (Array a, witness)
 fromList list b =
   list
     & Data.List.Linear.length
@@ -115,7 +117,7 @@ fromList list b =
     -- NOTE: Would be nice if this function would be made part of Data.List.Linear.
     listWithIndexes :: [a] %1 -> [(a, Int)]
     listWithIndexes = Unsafe.toLinear (\list -> Prelude.zip list [0 ..])
-    doWrites :: [(a, Int)] %1 -> Array a %1 -> Array a
+    doWrites :: HasCallStack => [(a, Int)] %1 -> Array a %1 -> Array a
     doWrites [] arr = arr
     doWrites ((a, ix) : xs) arr = doWrites xs (writeEmpty ix arr a)
     writeEmpty :: Int %1 -> Array a %1 -> a %1 -> Array a
@@ -123,7 +125,7 @@ fromList list b =
       arr
         -- SAFETY: Array has same size as input list
         & unsafeSwap ix a
-        -- SAFETY: The consumed element is a bottom:
+        -- SAFETY: The forgotten element is a bottom:
         & Unsafe.toLinear (\(_, arr) -> arr)
 
 -- | [Unique constructor]("Unique#unique_constructor") to create an `Array` with all elements initialized with the same value.
@@ -161,19 +163,19 @@ toList# = unArray# Prelude.$ \arr ->
 unArray# :: (GHC.MutableArray# GHC.RealWorld a -> b) -> Array# a %1 -> b
 unArray# f = Unsafe.toLinear (\(Array# a) -> f a)
 
-size :: Array a %1 -> (Ur Int, Array a)
+size :: Array a %1 -> (Int, Array a)
 size (Array arr) = f (size# arr)
   where
-    f :: (# Ur Int, Array# a #) %1 -> (Ur Int, Array a)
+    f :: (# Int, Array# a #) %1 -> (Int, Array a)
     f (# s, arr #) = (s, Array arr)
 
-size# :: Array# a %1 -> (# Ur Int, Array# a #)
+size# :: Array# a %1 -> (# Int, Array# a #)
 size# = Unsafe.toLinear go
   where
-    go :: Array# a -> (# Ur Int, Array# a #)
+    go :: Array# a -> (# Int, Array# a #)
     go (Array# arr) =
       let !s = GHC.sizeofMutableArray# arr
-       in (# Ur (GHC.I# s), Array# arr #)
+       in (# GHC.I# s, Array# arr #)
 
 swap :: HasCallStack => Int %1 -> a %1 -> Array a %1 -> (a, Array a)
 swap i val arr = dup2 i & \(i, i2) -> unsafeSwap i val (assertIndexInRange i2 arr)
@@ -201,22 +203,22 @@ unsafeSwap# = Unsafe.toLinear3 $ \(GHC.I# i) elem (Array# arr) ->
 
 -- | Get the value of an index. The index should be less than the arrays 'size',
 -- otherwise this errors.
-get :: (HasCallStack, Dupable a) => Int -> Array a %1 -> (a, Array a)
-get i arr = unsafeGet i (assertIndexInRange i arr)
+get :: (HasCallStack, Dupable a) => Int %1 -> Array a %1 -> (a, Array a)
+get i arr = dup i & \(i, i2) -> unsafeGet i (assertIndexInRange i2 arr)
 
 -- | Same as 'get', but does not do bounds-checking. The behaviour is undefined
 -- if an out-of-bounds index is provided.
-unsafeGet :: Dupable a => Int -> Array a %1 -> (a, Array a)
+unsafeGet :: Dupable a => Int %1 -> Array a %1 -> (a, Array a)
 unsafeGet ix (Array arr) = wrap (unsafeGet# ix arr)
   where
     wrap :: (# a, Array# a #) %1 -> (a, Array a)
     wrap (# ret, arr #) = (ret, Array arr)
 
-unsafeGet# :: Dupable a => Int -> Array# a %1 -> (# a, Array# a #)
-unsafeGet# (GHC.I# i) = Unsafe.toLinear go
+unsafeGet# :: Dupable a => Int %1 -> Array# a %1 -> (# a, Array# a #)
+unsafeGet# = Unsafe.toLinear2 go
   where
-    go :: Dupable a => Array# a -> (# a, Array# a #)
-    go (Array# arr) =
+    go :: Dupable a => Int -> Array# a -> (# a, Array# a #)
+    go (GHC.I# i) (Array# arr) =
       case GHC.runRW# (GHC.readArray# arr i) of
         (# _, ret #) -> dup ret & \(_, copy) -> (# copy, Array# arr #)
 {-# NOINLINE unsafeGet# #-} -- prevents the runRW# effect from being reordered
@@ -225,8 +227,8 @@ unsafeGet# (GHC.I# i) = Unsafe.toLinear go
 --
 -- Restricted to element types which are 'Consumable' because the old
 -- value is consumed in the process.
-set :: (Consumable a, HasCallStack) => Int -> a %1 -> Array a %1 -> Array a
-set ix val arr = unsafeSet ix val (assertIndexInRange ix arr)
+set :: (Consumable a, HasCallStack) => Int %1 -> a %1 -> Array a %1 -> Array a
+set ix val arr = dup ix & (\(ix, ix2) -> unsafeSet ix val (assertIndexInRange ix2 arr))
 
 -- | Same as 'set', but does not do bounds-checking. The behaviour is undefined
 -- if an out-of-bounds index is provided.
@@ -248,11 +250,17 @@ unsafeSet# = Unsafe.toLinear3 go
                   & (\() -> Array# arr)
 {-# NOINLINE unsafeSet# #-} -- prevents the runRW# effect from being reordered
 
-instance Consumable (Array a) where
-  consume (Array arr) = arr `lseq#` ()
+-- | Consumes the array by consuming all of its elements.
+instance Consumable a => Consumable (Array a) where
+  consume (Array arr) = consume# arr
+
+-- | When the elements of the array are allowed to be used unrestrictedly,
+-- consuming the array is a no-op (as individual elements do not need to be consumed in this case).
+instance Consumable (Array (Ur a)) where
+  consume = Unsafe.toLinear (const ())
 
 -- | Duplicating an array requires a deep copy.
-instance Dupable (Array a) where
+instance Dupable a => Dupable (Array a) where
   dup2 (Array arr) = wrap (dup2# arr)
     where
       wrap :: (# Array# a, Array# a #) %1 -> (Array a, Array a)
@@ -262,34 +270,58 @@ instance Dupable (Array a) where
 instance Unique (Array a)
 
 -- | Consume an 'Array#'.
+-- This calls `consume` on all elements.
 --
 -- Note that we can not implement a 'Consumable' instance because 'Array#'
 -- is unlifted.
-lseq# :: Array# a %1 -> b %1 -> b
-lseq# = Unsafe.toLinear2 (\_ b -> b)
+lseq# :: Consumable a => Array# a %1 -> b %1 -> b
+lseq# arr b = (consume# arr) & \() -> () `seq` b
+
+consume# :: Consumable a => Array# a %1 -> ()
+consume# arr = consume (toList# arr)
+{-# NOINLINE consume# #-}
 
 -- | Clone an Array#.
 --
 -- Note that we can not implement a 'Dupable' instance because 'Array#'
 -- is unlifted.
-dup2# :: Array# a %1 -> (# Array# a, Array# a #)
-dup2# = Unsafe.toLinear go
+dup2# :: Dupable a => Array# a %1 -> (# Array# a, Array# a #)
+dup2# arr = 
+    -- Approach: 
+    -- (1) Duplicate each element We now have an array of pairs.
+    -- (2) Clone the array. /!\ The clone now aliases the same pairs /!\.
+    -- (3) Use only the first pair elem in the first array and the second pair elem in the cloned array. Uniqueness is now restored.
+    Unsafe.toLinear go (map# dup2 arr)
+  where
+    cloneArr (Array# arr) = 
+        case GHC.runRW# (GHC.cloneMutableArray# arr 0# (GHC.sizeofMutableArray# arr)) of 
+            (#_, new #) -> Array# new
+    {-# NOINLINE cloneArr #-}
+    go :: Array# (a, a) -> (# Array# a, Array# a #)
+    go arr = (# map# unsafeFst arr, map# unsafeSnd (cloneArr arr) #)
+    unsafeFst :: (a, b) %1 -> a
+    unsafeFst = Unsafe.toLinear (\(a, _) -> a)
+    unsafeSnd :: (a, b) %1 -> b
+    unsafeSnd = Unsafe.toLinear (\(_, b) -> b)
+
+dup2Ur# :: Array# (Ur a) %1 -> (# Array# (Ur a), Array# (Ur a) #)
+dup2Ur# = Unsafe.toLinear go
   where
     go :: Array# a -> (# Array# a, Array# a #)
     go (Array# arr) =
       case GHC.runRW#
         (GHC.cloneMutableArray# arr 0# (GHC.sizeofMutableArray# arr)) of
         (# _, new #) -> (# Array# arr, Array# new #)
-{-# NOINLINE dup2# #-}
+{-# NOINLINE dup2Ur# #-}
 
 -- | Check if given index is within the Array, otherwise panic.
 assertIndexInRange :: Int %1 -> Array a %1 -> Array a
 assertIndexInRange i arr =
   dup2 i & \(i1, i2) ->
-    size arr & \(Ur s, arr') ->
+    size arr & \(s, arr') ->
       if 0 <= i1 && i2 < s
         then arr'
-        else arr' `lseq` error "Array: index out of bounds"
+        else arr' `unsafeLseq` error "Array: index out of bounds"
 
 -- | Map a linear function over an array.
 --
@@ -298,6 +330,7 @@ assertIndexInRange i arr =
 -- and thus we can re-use the input array's memory without doing extra allocation.
 map :: (a %1 -> b) -> Array a %1 -> Array b
 map f (Array arr) = Array (map# f arr)
+{-# INLINE[1] map #-}
 
 map# :: (a %1 -> b) -> Array# a %1 -> Array# b
 map# (f :: a %1 -> b) =
@@ -325,13 +358,20 @@ map# (f :: a %1 -> b) =
     )
 {-# NOINLINE map# #-}
 
+{-# RULES
+"map#/map#" forall f g. Unique.Array.map f . Unique.Array.map g = Unique.Array.map (f . g)
+ #-}
+
 consumingSum :: AddIdentity a => Array a %1 -> a
 consumingSum arr =
-  size arr & \(Ur s, arr') ->
-    go zero s arr'
+  size arr & \(s, arr) ->
+    go zero (move s) arr
   where
-    go :: AddIdentity a => a %1 -> Int -> Array a %1 -> a
-    go acc 0 arr = arr `lseq` acc
-    go acc i arr =
-      unsafeSwap (i - 1) (error "Empty placeholder element from `consumingSum`") arr & \(a, arr') ->
-        go (acc + a) (i - 1) arr'
+    go :: AddIdentity a => a %1 -> Ur Int %1 -> Array a %1 -> a
+    go acc (Ur 0) arr = arr `unsafeLseq` acc
+    go acc (Ur i) arr =
+        unsafeSwap (i - 1) (error "Empty placeholder element from `consumingSum`") arr & \(a, arr') ->
+            go (acc + a) (Ur (i - 1)) arr'
+
+unsafeLseq :: HasCallStack => a %1 -> b %1 -> b
+unsafeLseq = Unsafe.toLinear2 seq
